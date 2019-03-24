419f813eb651046eea7d30276dda9b3870eeea50
nzbenji <benlittle53@gmail.com>
2019-03-25 12:02:12 +1300

    added simple counter app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-const App = () => {
-  
+const App = (props) => {
+  const [count, setCount] = useState(props.count)
+  const [text, setText] = useState('')
+
   return (
     <div>
-      <p>Current count is 0</p>
+      <p>The current {text || 'count'} is {count} </p>
+      <button onClick={ () => setCount(count +1) }>+1</button>
+      <button onClick={ () => setCount(count -1) }>-1</button>
+      <button onClick={ () => setCount(props.count) }>Reset</button>
+      <input value={text} onChange={(e) => setText(e.target.value)}/>
     </div>
   )
+}
+
+App.defaultProps = {
+  count: 0
 }
 
 export default App;
