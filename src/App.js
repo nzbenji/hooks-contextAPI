24f195260947d4c2c1ec819d7478a89b0f4729fd
nzbenji <benlittle53@gmail.com>
2019-03-26 16:03:53 +1300

    Refactored note app into different files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import Notes from './Notes'
+import Notes from './components/NoteApp'
 
-const App = (props) => {
-  const [count, setCount] = useState(props.count)
-  const [text, setText] = useState('')
-
-  useEffect(() => {
-    console.log('run once')
-  })
-
-  useEffect(() => {
-    document.title = count
-  }, [count])
-
+const App = () => {
   return (
     <div>
-      <p>The current {text || 'count'} is {count} </p>
-      <button onClick={ () => setCount(count +1) }>+1</button>
-      <button onClick={ () => setCount(count -1) }>-1</button>
-      <button onClick={ () => setCount(props.count) }>Reset</button>
-      <input value={text} onChange={(e) => setText(e.target.value)}/>
-
-      <div>
         <Notes />
-      </div>
     </div>
   )
-}
-
-App.defaultProps = {
-  count: 0
 }
 
 export default App;
---
+++ src/components/NoteApp.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useReducer} from 'react'
+import notesReducer from '../reducer'
+import NoteList from './NoteList'
+import AddNoteForm from './AddNoteForm'
+
+const Notes = () => {
+    const [notes, dispatch] = useReducer(notesReducer, [])
+    
+    const removeNote = (title) => {
+        dispatch({
+            type: 'REMOVE_NOTE',
+            title
+        })
+    }
+
+    useEffect(() => {
+        const notes = JSON.parse(localStorage.getItem('notes'))
+
+        if (notes) {
+            dispatch({ type: 'POPULATE_NOTES', notes })
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes))
+    }, [notes])
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <NoteList notes={notes} removeNote={removeNote} />
+            <AddNoteForm dispatch={dispatch} />  
+        </div>
+    )
+}
+
+export default Notes
